a5567a9e17b7c05d28a3dbe2fe4376287471a6a0
andyyoyo <andyyoyo76543@gmail.com>
2016-04-06 18:08:38 +0800

    Update Bubble sort.java
    add the main method that user can enter the number

--- Bubble sort.java
+++ Bubble sort.java
@@ -23,3 +23,22 @@ public class bubbleSort {
 			}
 		}
 	}
+	//main method
+	public static void main(String[] args){
+		//prompt the user to enter the amount that user want
+		System.out.println("Enter the amount of the number that you want:");
+		Scanner input = new Scanner(System.in);
+		int a =input.nextInt();
+		System.out.println("Enter, please");
+		int[] b= new int[a];
+		for(int i=0;i<a;i++){
+			int c = input.nextInt();
+			System.out .println("Enter the next");
+			b[i] = c;
+		}
+		bubbleSort(b);
+		for(int i=0;i<b.length;i++){
+			System.out.println(b[i]+"");
+		}
+	}
+}
